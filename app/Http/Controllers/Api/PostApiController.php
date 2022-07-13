baac317564c789584dcfcf96984a223eb1bb7666
Bruno Bernardini <bruno.f.bernardini@example.com>
2022-07-14 00:28:29 +0200

    API che genera JSON contenente tutti i post.

---
+++ app/Http/Controllers/Api/PostApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostApiController extends Controller
+{
+    public function index()
+    {
+        $data = Post::all();
+        return response()->json($data);
+    }
+}
